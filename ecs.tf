ed8de91d1ecdc2423df3cccb30bd03153b53707b
Nick Evans <nick.evans@northwestern.edu>
2026-06-17 13:25:16 -0500

    Enable SMTP usernmae-based tagging

--- ecs.tf
+++ ecs.tf
@@ -42,7 +42,8 @@ resource "aws_ecs_task_definition" "mailhog" {
     },
     "environment": [
         {"name": "MH_HOSTNAME", "value": "${var.hostname}"},
-        {"name": "MH_STORAGE", "value": "memory"}
+        {"name": "MH_STORAGE", "value": "memory"},
+        {"name": "MP_TAGS_USERNAME", value: "true"}
     ]
 }]
 DEFINITION
